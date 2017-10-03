d0b5b3ab66638bbac81793ff1789582b09b7fd21
Lubomir Ontkoc <xontkoc@fi.muni.cz>
2017-10-03 11:47:01 +0200

    Add West-Life theme for SSP

---
+++ themes/west-life/default/includes/footer.php
@@ -0,0 +1,57 @@
+<?php
+if(!empty($this->data['htmlinject']['htmlContentPost'])) {
+	foreach($this->data['htmlinject']['htmlContentPost'] AS $c) {
+		echo $c;
+	}
+}
+?>
+    <link rel="mask-icon" href="images/instruct.svg" color="#54585a">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,600,400italic,300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/lib/select2.min.35673.css">
+    <link rel="stylesheet" href="css/lib/bootstrap-tagsinput.37701.css">
+    <link rel="stylesheet" href="css/lib/redactor.90770.css">
+    <link rel="stylesheet" href="css/lib/font-awesome.min.82376.css">
+    <link rel="stylesheet" href="css/lib/imgareaselect-animated.51567.css">
+    <link rel="stylesheet" href="css/lib/bootstrap-tagsinput.83436.css">
+    <link rel="stylesheet" href="css/cupertino/jquery-ui.min.76770.css">
+    <link rel="stylesheet" href="css/core.reset.69248.css">
+    <link rel="stylesheet" href="css/core.grid.54964.css">
+    <link rel="stylesheet" href="css/core.files.62758.css">
+    <link rel="stylesheet" href="css/core.forms.47184.css">
+    <link rel="stylesheet" href="css/core.palette.50215.css">
+    <link rel="stylesheet" href="css/core.tables.59672.css">
+    <link rel="stylesheet" href="css/style.13818.css">
+    <link rel="stylesheet" href="css/core.admin.bar.92757.css">
+    <link rel="stylesheet" href="css/core.admin.pages.74395.css">
+    <link rel="stylesheet" href="css/core.print.92597.css" media="print">
+    <link rel="stylesheet" href="css/core.notifications.20139.css">
+    <link rel="stylesheet" href="css/core.menu.65149.css">
+    <link rel="stylesheet" type="text/css" href="https://auth.west-life.eu/proxy/module.php/west-life/res/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="https://auth.west-life.eu/proxy/module.php/west-life/res/css/west-life.css" />
+
+</div><!-- #content -->
+</div><!-- #wrap -->
+
+<div id="footer">
+
+    <div style="margin: 0px auto; max-width: 1000px;">
+        <div class="row">
+      <div class="col-sm-2">
+        <img src="http://auth.west-life.eu/images/ec.png" alt="eu-flag" style="max-width: 120px;">
+      </div>
+      <div class="col-sm-9">
+        <p>Copyright &copy; 2017 West-Life<br>
+          This project is funded by Horizon 2020<br>
+          West-Life is part of the e-Infrastructure Virtual Research Environment (VRE) project No. 675858
+        </p>
+      </div>
+    </div>
+
+    </div>
+
+
+</div><!-- #footer -->
+
+</body>
+</html>
+
